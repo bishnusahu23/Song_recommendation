fb114bc059124fa226b4bfa3a2bcf61adafa1d31
Bks <sahu.bishnukumari@gmail.com>
2024-08-28 21:26:33 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pickle
+import streamlit as st
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Initialize the Spotify client
+CLIENT_ID = "4858241d3e1e4897ae23adb111fdb00f"
+CLIENT_SECRET = "efebedcd3c1842bea2b9ffe30e1b8a2f"
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def get_song_album_cover_url(song_name, artist_name):
+    search_query = f"track:{song_name} artist:{artist_name}"
+    results = sp.search(q=search_query, type="track")
+
+    if results and results["tracks"]["items"]:
+        track = results["tracks"]["items"][0]
+        album_cover_url = track["album"]["images"][0]["url"]
+        return album_cover_url
+    else:
+        return "https://i.postimg.cc/0QNxYz4V/social.png"
+
+def recommend(Name,top_n):
+    index = music[music['Name'] == Name].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_music_names = []
+    recommended_music_posters = []
+    for i in distances[1:top_n+1]:
+        artist = music.iloc[i[0]].Artist
+        recommended_music_posters.append(get_song_album_cover_url(music.iloc[i[0]].Name, artist))
+        recommended_music_names.append(music.iloc[i[0]].Name)
+
+    return recommended_music_names, recommended_music_posters
+
+st.markdown("<h1 style='text-align: center; color: #4CAF50;'>Music Recommendation System</h1>", unsafe_allow_html=True)
+
+music = pickle.load(open('final_tracks.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+music_list = music['Name'].values
+selected_song = st.selectbox("Type or select a song from the dropdown", music_list)
+# Select the number of recommendations
+number_of_songs = st.slider('Select number of recommended songs:', 1, 10, 5)
+
+if st.button('Show Recommendation'):
+    recommended_music_names, recommended_music_posters = recommend(selected_song,number_of_songs)
+    st.subheader('Recommended Songs for You')
+    
+    
+    for name, poster in zip(recommended_music_names, recommended_music_posters):
+        with st.container():
+            col1, col2 = st.columns([1, 4])  
+            with col1:
+                st.image(poster, width=80)   
+            with col2:
+                st.write(name)                
+                st.markdown('---')     
+# In[ ]:
+
+
+
+
